936a154da5540fd0f9795209464d5bfc16283364
nalina <naleenameshram85@gmail.com>
2026-03-03 15:45:36 +0530

    working

---
+++ Frontend/XEventApp/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import React from "react";
+function App() {
+  return (
+    <div className="text-3xl font-bold underline bg-slate-500">
+      <h2>Hello World!!</h2>
+    </div>
+  );
+}
+
+export default App;
